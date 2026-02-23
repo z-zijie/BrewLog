b81b28cad3d2ba1b16c386d09fbea5595bd13ac1
Zijie Zhang <44576728+z-zijie@users.noreply.github.com>
2026-02-23 22:02:03 +0800

    Initial Commit

---
+++ BrewLog/BrewLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewLogApp.swift
+//  BrewLog
+//
+//  Created by Zijie Zhang on 2026/2/23.
+//
+
+import SwiftUI
+
+@main
+struct BrewLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BrewLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BrewLog
+//
+//  Created by Zijie Zhang on 2026/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
